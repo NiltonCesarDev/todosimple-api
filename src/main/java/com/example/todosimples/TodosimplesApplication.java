0ab21707fa3b2a8be6cfa4ef699835a84e89f4b0
NiltonCesarDev <ncbombtab@outlook.com>
2026-03-08 20:31:43 -0300

    Configure project

--- src/main/java/com/example/todosimples/TodosimplesApplication.java
+++ src/main/java/com/example/todosimples/TodosimplesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodosimplesApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(TodosimplesApplication.class, args);
+        SpringApplication.run(TodosimplesApplication.class, args);
 	}
-
 }
